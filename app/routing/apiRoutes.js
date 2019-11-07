ba187f99ae28781a9d62b24af58d39333c1c68d3
Sable Hoover <SableVonHoover@gmail.com>
2019-11-07 13:24:21 -0600

    updated survey js

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -10,7 +10,7 @@ module.exports = function (app) {
 
     //Checking the user's score array against friends in friends.js
     app.post("/api/friends", function (req, res) {
-        var user = req.body;
+        var User = req.body;
         var match = {};
         var ceiling = 999999999;
 
@@ -20,7 +20,7 @@ module.exports = function (app) {
 
             //Finding difference in score between user and friends
             for (i = 0; i < 10; i++) {
-                total += Math.abs(user.scores[i] - friend.scores[i]);
+                total += Math.abs(User.scores[i] - friend.scores[i]);
             }
 
             //Updating the match obj every time a friend with a closer score is found
@@ -37,6 +37,6 @@ module.exports = function (app) {
         });
 
         //Adding the user's answers to the friends.js
-        friends.push(user)
+        friends.push(User)
     });
 }
