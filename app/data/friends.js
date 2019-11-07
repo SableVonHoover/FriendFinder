af95d7197abb5d3b1aef328d7ebe713edf893325
Sable Hoover <SableVonHoover@gmail.com>
2019-11-07 17:10:33 -0600

    updated friends js

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,53 +1,53 @@
 friends = [
     {
-        name: "",
-        pic: "",
-        scores: []
+        name: "Bob",
+        pic: "https://i.dailymail.co.uk/i/pix/2013/08/29/article-2405475-1B8389EE000005DC-718_634x550.jpg",
+        scores: [2, 4, 1, 3, 4, 2, 1, 2, 3, 4]
     },
     {
-        name: "",
-        pic: "",
-        scores: []
+        name: "Beep",
+        pic: "https://i.imgur.com/XDGhaki.jpg",
+        scores: [3, 1, 3, 3, 2, 1, 5, 4, 1, 3]
     },
     {
-        name: "",
-        pic: "",
-        scores: []
+        name: "Bjorn",
+        pic: "https://i.pinimg.com/originals/32/38/6c/32386c72c7f2a8b5c1a10fc51c149cb1.jpg",
+        scores: [5, 3, 4, 2, 5, 3, 1, 2, 1, 3]
     },
     {
-        name: "",
-        pic: "",
-        scores: []
+        name: "Billy",
+        pic: "https://fiverr-res.cloudinary.com/images/t_main1,q_auto,f_auto/gigs/3015823/original/Bill-Cosby-Autographed-8x10/rub-whip-cream-over-my-body-while-screaming-nicolas-cage-10-times.jpg",
+        scores: [2, 3, 4, 2, 1, 1, 3, 4, 5, 1]
     },
     {
-        name: "",
-        pic: "",
-        scores: []
+        name: "BillyBob",
+        pic: "http://i.cubeupload.com/EbWc6J.jpg",
+        scores: [1, 2, 3, 1, 5, 2, 3, 4, 5, 1]
     },
     {
-        name: "",
-        pic: "",
-        scores: []
+        name: "Ben",
+        pic: "https://pm1.narvii.com/5866/45cc61eb9e3eaefdc26be045e9f9fbfdf916f45b_hq.jpg",
+        scores: [2, 1, 2, 3, 4, 1, 5, 2, 1, 2]
     },
     {
-        name: "",
-        pic: "",
-        scores: []
+        name: "Brooke",
+        pic: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSySVRkhMpkEByVsJz-UDcr3OmIin-uWHnWyfpkwQvQk-uc-YqJ&s",
+        scores: [5, 3, 1, 3, 2, 2, 1, 3, 4, 1]
     },
     {
-        name: "",
-        pic: "",
-        scores: []
+        name: "Blake",
+        pic: "https://icps.doctor/wp-content/uploads/userpro/23/5b3f36c62c0d4.jpg",
+        scores: [1, 2, 3, 4, 5, 3, 2, 1, 2, 3]
     },
     {
-        name: "",
-        pic: "",
-        scores: []
+        name: "Beau",
+        pic: "https://v.cdn.vine.co/r/thumbs/7BBFD9F6741001009555343618048_14375368a64.3.4.mp4_.h0bQhclekDe.d93NrKYlAAnFPh_0MZ4ytGv6n2jVNfDUCsiiScO0NCYXe4Lt2ER.jpg?versionId=hxKBukyR5.deHYpG1a.qOKnPNUD5HgEN",
+        scores: [5, 3, 1, 2, 3, 1, 2, 3, 4, 1]
     },
     {
-        name: "",
-        pic: "",
-        scores: []
+        name: "Barnaby",
+        pic: "https://external-preview.redd.it/Iv3q3gGJgr_KrxL4bMf06xNNuJksjJiyTI-5umEdGvo.jpg?auto=webp&s=085329d638f83bc1a1a98bca74a46e70679bfb76",
+        scores: [1, 2, 3, 4, 2, 1, 2, 4, 2, 4]
     }
 ];
 
